2f09058290c0fa9ca5454a67273ea79e84c40f45
Fedriek Chandra <fedriek.chandra@stickearn.com>
2022-07-14 01:40:56 +0700

    init user & token API

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function init()
+    {
+        $data = request()->validate([
+            'customer_xid' => ['required','uuid']
+        ]);
+
+        $user = User::query()->firstOrCreate([
+            'id' => $data['customer_xid']
+        ]);
+
+        $token = $user->createToken($user->id)->plainTextToken;
+        $data = compact('token');
+        $resp = apiResp($data);
+
+        return response()->json($resp);
+    }
+}
